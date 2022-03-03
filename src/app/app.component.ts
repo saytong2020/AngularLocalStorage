f1e0608a0fab07f6f8132a52f46ce917f701b7f1
TongSay <tongsayofficail@gmail.com>
2022-03-03 10:33:20 +0700

    UpdateLocalStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +10,9 @@ import { NgForm, FormGroup, FormControl, FormBuilder, Validators } from '@angula
 export class AppComponent implements OnInit {
 
   
-   eusers = [];
+   users = [];
   
-   registerForm: any;
+   registerForm:any;
 
    constructor(private fb:FormBuilder){
      
@@ -24,17 +26,15 @@ export class AppComponent implements OnInit {
         name: ['',Validators.required],
         username: ['',Validators.required],
         email: ['',Validators.email],
-        phone: ['',[Validators.required,Validators.minLength(9)]],
+        phone: ['',[Validators.required,Validators.minLength(9),Validators.maxLength(10)]],
       }
     );
 
-    let users:any =  localStorage.getItem("$user_info");
-  
-    let eusers = JSON.parse(users);
-
-    this.eusers = eusers;
+    let esers:any = localStorage.getItem("$user_info");
+    let users = JSON.stringify(esers);
+    //this.users = users;
     
-   // console.log("Resalt:"+ eusers[0]['name']);
+   
    
   }
 
@@ -43,42 +43,34 @@ export class AppComponent implements OnInit {
   createUser()
   {
 
-    // console.log(form.value);
-
-    // let eusers:any = [];
-
-    // eusers = this.eusers;
-
-    // eusers.push(form.value);
-
-    // let user = JSON.stringify(eusers);
-
-    // localStorage.setItem("$user_info", user);
-
-    // form.resetForm();
-
     console.log(this.registerForm.getRawValue());
+
+    let userForm:any = this.registerForm.getRawValue();
+    let users:any = [];
+
+    users = this.users;
+
+    users.push(userForm);
+
+    let user = JSON.stringify(users);
     
+    localStorage.setItem("$user_info", user);
+
+    this.registerForm.reset();
   }
-
-
-
     deleteUser(index:any){
 
       // 1. get index 
       // 2. find items
 
 
-      let ind = this.eusers.indexOf(this.eusers[index]);
+      let ind = this.users.indexOf(this.users[index]);
 
       if (ind >-1){
-        this.eusers.splice(ind,1)
+        this.users.splice(ind,1)
       }
-      localStorage.setItem("$user_info",JSON.stringify(this.eusers) );
+      localStorage.setItem("$user_info",JSON.stringify(this.users) );
       console.log(ind);
 
     }
-
- 
-
 }
